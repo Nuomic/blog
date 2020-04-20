20669ae8526317fd3c77099992967cb8ecfdf161
zhangweiqiang666 <972618478@qq.com>
2020-04-20 21:06:53 +0800

    文章详情更改

--- src/pagesFr/articledetail/View.js
+++ src/pagesFr/articledetail/View.js
@@ -1,21 +1,30 @@
 import React from 'react';
 import BasicLayout from '../components/BasicLayout';
-import { Card, Typography, Tag } from 'antd';
+import { Card, Typography, Tag, Icon } from 'antd';
 import ReactMarkdown from 'react-markdown';
 import Comments from '../components/Comments';
 import { Link, Style } from 'react-imvc/component';
 import CodeBlock from './component/CodeBlock';
+import moment from 'moment';
 const { Title, Paragraph } = Typography;
-export default props => {
+export default (props) => {
   console.log('props', props);
 
-  const { articleDetail } = props.state;
+  const { articleDetail = {} } = props.state;
   const { categoryInfo, next, pre, tagList } = articleDetail;
   const BreadcrumbList = [
     { name: '首页', href: '/home' },
     { name: '博文', href: '/home' },
-    { name: articleDetail.title }
+    { name: articleDetail.title },
   ];
+  const IconText = ({ type, text, style = {}, onClick }) => {
+    return (
+      <span style={{ padding: '0 20px 0 0', fontSize: 13 }} onClick={onClick}>
+        <Icon type={type} style={{ marginRight: 8, ...style }} />
+        <span style={{ color: '#eee' }}>{text}</span>
+      </span>
+    );
+  };
   return (
     <BasicLayout BreadcrumbList={BreadcrumbList}>
       <Card
@@ -24,12 +33,18 @@ export default props => {
             <Title level={4} ellipsis>
               {articleDetail.title}
             </Title>
-            <span>{categoryInfo.name}</span>
-            <span>{articleDetail.date}</span>
-            <span>{categoryInfo.name}</span>
-            <span>{categoryInfo.name}</span>
+            <IconText type="unordered-list" text={categoryInfo.name} />
+            <IconText type="eye" text={articleDetail.viewCount} />
+            <IconText type="like" text={articleDetail.likeCount} />
+            <IconText
+              type="calendar"
+              text={moment(articleDetail.createdAt).format(
+                'YYYY-MM-DD HH:mm:ss'
+              )}
+            />
+
             <div>
-              {tagList.map(item => (
+              {tagList.map((item) => (
                 <Tag key={item.id} color={item.color}>
                   {item.name}
                 </Tag>
@@ -44,7 +59,7 @@ export default props => {
           source={articleDetail.content}
           escapeHtml={false}
           renderers={{
-            code: CodeBlock
+            code: CodeBlock,
           }}
         />
       </Card>
